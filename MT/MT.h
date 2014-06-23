1173855cc1f6e978bdfe8adc0d5db3f7e7877f6c
yan qing <xyzs996@gmail.com>
2014-06-23 16:46:07 +0800

    aa

---
+++ MT/MT.h
@@ -0,0 +1,14 @@
+//
+//  MT.h
+//  MT
+//
+//  Created by YanQing on 14-5-27.
+//  Copyright (c) 2014年 YanQing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MT : NSObject
++(void)back;
+
+@end
---
+++ MT/MT.m
@@ -0,0 +1,16 @@
+//
+//  MT.m
+//  MT
+//
+//  Created by YanQing on 14-5-27.
+//  Copyright (c) 2014年 YanQing. All rights reserved.
+//
+
+#import "MT.h"
+
+@implementation MT
++(void)back
+{
+    NSLog(@"这个是一个测试程序");
+}
+@end
